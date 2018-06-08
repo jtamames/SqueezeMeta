da08581780193b4d14498ca42bd47e0d908c066b
fpusan <fpusan@gmail.com>
2018-06-08 16:51:04 +0200

    add ; when adding dbpath to conf file

--- scripts/preparing_databases/make_databases.pl
+++ scripts/preparing_databases/make_databases.pl
@@ -88,7 +88,7 @@ close outfile1;
 open(outfile2,">$installpath/scripts/squeezeM_conf.pl") || die;
 open(infile1, "$installpath/scripts/squeezeM_conf_original.pl") || die;
 while(<infile1>) {
-	if($_=~/^\$databasepath/) { print outfile2 "\$databasepath=\"$databasedir\"\n"; }
+	if($_=~/^\$databasepath/) { print outfile2 "\$databasepath=\"$databasedir\";\n"; }
 	else { print outfile2 $_; }
 	}
 close infile1;
