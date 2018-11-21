1a6046298f55186321ec1cceed27a0464529f9cb
Fernando Puente-Sánchez <fpusan@gmail.com>
2018-11-21 11:18:48 +0100

    clean

--- scripts/preparing_databases/parents.sql
+++
@@ -1,5 +0,0 @@
-create table parents(id TEXT PRIMARY KEY, fulltax TEXT, taxonomy_id INT);
-.separator "\t"
---.import parents.txt parents
-.exit
-
--- scripts/preparing_databases/taxid.sql
+++
@@ -1,4 +0,0 @@
-create table taxid(id TEXT PRIMARY KEY, species TEXT, genus TEXT,family TEXT, rorder TEXT,class TEXT,phylum TEXT,supekingdom TEXT);
---.separator "\t"
---.import taxid_tree.txt taxid
-.exit
