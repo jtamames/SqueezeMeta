4380000aaf12e09d9422e7267d69d4d06f2cf635
Fernando Puente-Sánchez <fpusan@gmail.com>
2019-02-01 11:35:30 +0100

    add timestamp on db creation

--- scripts/preparing_databases/make_databases.pl
+++ scripts/preparing_databases/make_databases.pl
@@ -6,7 +6,7 @@ my $REMOVE_NR=1;
 my $REMOVE_TAXDUMP=1;
 my $REMOVE_LCA_TAX_INTERMEDIATE=1;
 
-###scriptdir patch, Fernando Puente-Sánchez, 07-V-2018
+### scriptdir patch, Fernando Puente-Sánchez, 07-V-2018
 use File::Basename;
 our $dbscriptdir = dirname(__FILE__);
 our $installpath = "$dbscriptdir/../..";
@@ -23,44 +23,44 @@ if(!$download_dir) { die "Usage: perl make_databases.pl <download dir>\n"; }
 system("rm $download_dir/test.tar.gz $libpath/classifier.tar.gz $download_dir/db.tar.gz $download_dir/kegg.dmnd.gz");
 
 
-###Download test data (-U '' so that we give the server an user agent string, it complains otherwise).
+### Download test data (-U '' so that we give the server an user agent string, it complains otherwise).
 print "\nDownloading and unpacking test data...\n\n";
 system("wget -U '' -P $download_dir http://wwwuser.cnb.csic.es/~squeezem/test.tar.gz; tar -xvzf $download_dir/test.tar.gz -C $download_dir; rm $download_dir/test.tar.gz");
 
 
-###Download rdp classifier.
+### Download rdp classifier.
 print("Downloading and unpacking RDP classifier...\n");
 system("wget -U '' -P $libpath http://wwwuser.cnb.csic.es/~squeezem/classifier.tar.gz; tar -xvzf $libpath/classifier.tar.gz -C $libpath; rm $libpath/classifier.tar.gz");
 
 
-###Download general db tarball. (-U '' so that we give the server an user agent string, it complains otherwise)
+### Download general db tarball. (-U '' so that we give the server an user agent string, it complains otherwise)
 print "Downloading and unpacking general database tarball...\n";
 system("wget -U '' -P $download_dir http://wwwuser.cnb.csic.es/~squeezem/db.tar.gz; tar -xvzf $download_dir/db.tar.gz -C $download_dir; rm $download_dir/db.tar.gz");
 
 
-###Download and create kegg db.
+### Download and create kegg db.
 print "\nDownloading and creating kegg database...\n\n";
 system("wget -U '' -P $database_dir http://wwwuser.cnb.csic.es/~squeezem/kegg.db.gz; gunzip $database_dir/kegg.db.gz");
 system("$installpath/bin/diamond makedb --in $database_dir/kegg.db -d $database_dir/keggdb -p 8");
 system("rm $database_dir/kegg.db");
 
 
-###Download and create nr db.
+### Download and create nr db.
 print "Downloading and creating nr database. This will take a while (several hours)...\n";
 system "perl $dbscriptdir/make_nr_db.pl $database_dir";
 
 
-###Download and create eggnog db.
+### Download and create eggnog db.
 print "\nDownloading and creating eggnog database...\n\n";
 system "perl $dbscriptdir/make_eggnog_db.pl $database_dir";
 
 
-###Download and create Pfam db.
+### Download and create Pfam db.
 print "\nDownloading Pfam database...\n\n";
 system "wget -P $database_dir ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.hmm.gz; gunzip $database_dir/Pfam-A.hmm.gz";
 
 
-###Create LCA database from nr data.
+### Create LCA database from nr data.
 print "\nCreating lca.db...\n";
 system "rm $lca_dir";
 system "mkdir $lca_dir";
@@ -91,9 +91,13 @@ if($REMOVE_TAXDUMP) { system("rm $lca_dir/*dmp $lca_dir/new_taxdump.tar.gz"); }
 if($REMOVE_LCA_TAX_INTERMEDIATE) { system("rm $lca_dir/nr.taxlist.db $lca_dir/taxid_tree.txt $lca_dir/taxatree.txt"); }
 
 
-###Update configuration files to reflect new db path.
-print("\nUpdating configuration...\n");
+### Report db creation date.
+my $timestamp = scalar localtime;
+my $time_command = "echo \"Finished database creatin at > $database_dir";
+system("echo  \"Finished database creation on $timestamp.\" > $database_dir/DB_BUILD_DATE");
 
+### Update configuration files to reflect new db path.
+print("\nUpdating configuration...\n");
 
 my $checkm_manifest = "{\"dataRoot\": \"$database_dir\", \"remoteManifestURL\": \"https://data.ace.uq.edu.au/public/CheckM_databases/\", \"manifestType\": \"CheckM\", \"localManifestName\": \".dmanifest\", \"remoteManifestName\": \".dmanifest\"}\n";
 
