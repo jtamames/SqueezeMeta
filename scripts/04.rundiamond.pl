dcf5675437e2246b297f031c3143fcb23bbf0c7d
jtamames <jtamames@cnb.csic.es>
2019-11-27 10:36:30 +0100

    Memory management in Diamond

--- scripts/04.rundiamond.pl
+++ scripts/04.rundiamond.pl
@@ -5,6 +5,7 @@
 
 use strict;
 use Cwd;
+use Linux::MemInfo;
 use lib ".";
 
 $|=1;
@@ -27,9 +28,35 @@ my $command;
 
 open(outmet,">>$methodsfile") || warn "Cannot open methods file $methodsfile for writing methods and references\n";
 open(outsyslog,">>$syslogfile") || warn "Cannot open syslog file $syslogfile for writing the program log\n";
+
+	#-- Setting block size for Diamond
+
+if($blocksize eq "NF") {
+	print "Setting block size for Diamond\n";
+	my %mem=get_mem_info;
+	my $ram=$mem{"MemFree"};
+	my $block_size_set=int($ram/5000000);
+	if($block_size_set>8) { $block_size_set=8; }	
+	if($block_size_set<1) { $block_size_set=8; }
+	print "AVAILABLE (free) RAM memory: $ram\nWe will set Diamond block size to $block_size_set (Gb RAM/6, Max 8). You can override this setting using the -b option\n";
+	print outsyslog "Diamond block size set to $block_size_set (Free Mem $ram bytes)\n";
+	$blocksize=$block_size_set;
+	}
+
 print outmet "Similarity searches for ";
 
 print "Running Diamond (Buchfink et al 2015, Nat Methods 12, 59-60) for";
+
+#-- nr database
+
+$command="$diamond_soft blastp -q $aafile -p $numthreads -d $nr_db -e $evaluetax4 --id $minidentax4 -f tab -b $blocksize --quiet -o $taxdiamond";
+print " taxa";
+print outsyslog "Running Diamond for taxa: $command\n";
+my $ecode = system $command;
+if($ecode!=0) { die "Error running command:    $command"; }
+print outmet "GenBank (Clark et al 2016, Nucleic Acids Res 44, D67-D72), ";
+print outmet "were done using Diamond (Buchfink et al 2015, Nat Methods 12, 59-60)\n";
+
 #-- COG database
 
 if(!$nocog) {
@@ -69,15 +96,7 @@ if($opt_db) {
 		}
 }
 
-#-- nr database
-
-$command="$diamond_soft blastp -q $aafile -p $numthreads -d $nr_db -e $evaluetax4 --id $minidentax4 -f tab -b $blocksize --quiet -o $taxdiamond";
-print " taxa\n";
-print outsyslog "Running Diamond for taxa: $command\n";
-my $ecode = system $command;
-if($ecode!=0) { die "Error running command:    $command"; }
-print outmet "GenBank (Clark et al 2016, Nucleic Acids Res 44, D67-D72), ";
-print outmet "were done using Diamond (Buchfink et al 2015, Nat Methods 12, 59-60)\n";
+print "\n";
 close outmet;
 close outsyslog;
 
