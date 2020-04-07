4fa09fb08ba00816d3e09ec25713c466063fb6a2
tamames <jtamames@cnb.csic.es>
2020-04-07 12:43:24 +0200

    Fixing missing annotations in lca multi

--- scripts/06.lca.pl
+++ scripts/06.lca.pl
@@ -272,7 +272,7 @@ sub query {
 		my $maxp=0;
 		foreach my $t(sort { $accum{$k}{$a}<=>$accum{$k}{$b}; } keys %{ $accum{$k} }) {
 			if($t && ($accum{$k}{$t}==$maxp)) { $lasttax=""; next; }    #-- Equality of hits, don´t choose any
-			$maxp=$accum{$k}{$t};
+			if($t) { $maxp=$accum{$k}{$t}; }
 			print "      $t $accum{$k}{$t}\n" if $verbose;
 			if(($accum{$k}{$t}>=$required) && ($accum{$k}{$t}>=$minreqhits) && ($required>0) && ($parents{$t}{wranks})) { 	#-- REQUIREMENTS FOR VALID LCA
 				print "$k -> $t\n" if $verbose;
