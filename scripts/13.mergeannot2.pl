a8719b15a444f3be72d8dd22404b4d94c428435b
Fernando Puente-Sánchez <fpusan@gmail.com>
2020-07-08 22:51:39 +0200

    Don't add +1 to aminoacid and nucleotide lengths in table 13

--- scripts/13.mergeannot2.pl
+++ scripts/13.mergeannot2.pl
@@ -224,7 +224,7 @@ while(<infile3>) {
 	if($_=~/^\>([^ ]+)/) {		#-- If we are reading a new ORF, store the data for the last one
 		if($aaseq) { 
 			$orfdata{$thisorf}{aaseq}=$aaseq; 
-			$orfdata{$thisorf}{length}=(length $aaseq)+1; 
+			$orfdata{$thisorf}{length}=(length $aaseq); 
 			$orfdata{$thisorf}{molecule}="CDS";
 			$orfdata{$thisorf}{method}="Prodigal";
 			}
@@ -237,7 +237,7 @@ close infile3;
 
 if($aaseq) { 
 	$orfdata{$thisorf}{aaseq}=$aaseq; 
-	$orfdata{$thisorf}{length}=(length $aaseq)+1; 
+	$orfdata{$thisorf}{length}=(length $aaseq); 
 	$orfdata{$thisorf}{molecule}="CDS";
 	$orfdata{$thisorf}{method}="Prodigal";
 	}
@@ -256,7 +256,7 @@ foreach my $thisntfile(@ntfiles) {
 		chomp;
 		if($_=~/^\>([^ ]+)/) {		#-- If we are reading a new ORF, store the data for the last one
 			if($ntseq) { 
-				$orfdata{$thisorf}{lengthnt}=(length $ntseq)+1; 
+				$orfdata{$thisorf}{lengthnt}=(length $ntseq); 
 				}
 			$thisorf=$1;
 			$ntseq="";
@@ -266,12 +266,12 @@ foreach my $thisntfile(@ntfiles) {
 	close infile3;
 
 	if($ntseq) {
-        	$orfdata{$thisorf}{lengthnt}=(length $ntseq)+1;
+        	$orfdata{$thisorf}{lengthnt}=(length $ntseq);
         	}
 	}
 	
 if($ntseq) { 
-	$orfdata{$thisorf}{lengthnt}=(length $ntseq)+1; 
+	$orfdata{$thisorf}{lengthnt}=(length $ntseq); 
 	}
 
 
@@ -287,7 +287,7 @@ while(<infile4>) {
 		my @mt=split(/\t/,$_);
 		if($rnaseq) { 
 			$orfdata{$thisrna}{ntseq}=$rnaseq;
-			$orfdata{$thisrna}{lengthnt}=(length $rnaseq)+1;
+			$orfdata{$thisrna}{lengthnt}=(length $rnaseq);
 			$orfdata{$thisorf}{length}="NA";
 			$orfdata{$thisrna}{molecule}="rRNA";
 			$orfdata{$thisrna}{method}="barrnap";
@@ -304,7 +304,7 @@ while(<infile4>) {
 close infile4;
 if($rnaseq) { 
 	$orfdata{$thisrna}{ntseq}=$rnaseq; 
-	$orfdata{$thisrna}{lengthnt}=(length $rnaseq)+1;
+	$orfdata{$thisrna}{lengthnt}=(length $rnaseq);
 	$orfdata{$thisrna}{molecule}="rRNA";
 	$orfdata{$thisrna}{length}="NA";
 	$orfdata{$thisrna}{method}="barrnap";
