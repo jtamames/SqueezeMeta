689add58bca1b833ccdf842f5dc181da445a21f8
Fernando Puente-Sánchez <fpusan@gmail.com>
2021-01-25 19:07:20 +0100

    Use contig taxonomy for nofilter and prokfilter
    Fixes an issue in which reads mapping to contigs but not ORFs were
    treated as "Unmapped" when creating the nofilter and prokfilter tables.
    Now we use contig taxonomy, which is The Way. This is enabled by 3833050c1349a82db28c45bc2d38c424d0706fb8

--- lib/SQMtools/R/loadSQM.R
+++ lib/SQMtools/R/loadSQM.R
@@ -239,11 +239,11 @@ loadSQM = function(project_path, tax_mode = 'allfilter', trusted_functions_only 
     cat('    taxonomy...\n')
     if(engine == 'data.frame')
         {
-        SQM$contigs$tax           = as.matrix(read.table(sprintf('%s/results/tables/%s.contig.tax.tsv', project_path, project_name),
+        SQM$contigs$tax           = as.matrix(read.table(sprintf('%s/results/tables/%s.contig.tax.%s.tsv', project_path, project_name, tax_mode),
                                                          header=T, row.names=1, sep='\t'))
     }else if(engine == 'data.table')
         {
-        ta                        = data.table::fread(sprintf('%s/results/tables/%s.contig.tax.tsv', project_path, project_name), header=T, sep='\t')
+        ta                        = data.table::fread(sprintf('%s/results/tables/%s.contig.tax.%s.tsv', project_path, project_name, tax_mode), header=T, sep='\t')
         SQM$contigs$tax           = as.matrix(ta[,-1])
         rownames(SQM$contigs$tax) = unlist(ta[,1])
         }
