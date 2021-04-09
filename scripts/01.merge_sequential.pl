051e854b279a4c88ecc978be7c5b424e112de83b
Javier Tamames <34687997+jtamames@users.noreply.github.com>
2021-04-09 10:11:19 +0200

    Update 01.merge_sequential.pl

--- scripts/01.merge_sequential.pl
+++ scripts/01.merge_sequential.pl
@@ -202,7 +202,7 @@ sub parseafg {
 			shift @m; shift @m;
 			# my $ts=$m[$#m];
 			 # $ts=join("_",@m);
-			 $ts=$m[$#m];
+			 $ts=$m[$#m];   #-- Last field of contig name contains the sample ID. Previous solution worked for megahit but not for SPAdes. This one can get trouble if sample names contains "_"
 			}
 		$order{$inpos}=$ts;
 		$samples{$ts}=1;
