7a5645f0472c4ae3a1bf7051a762a4c45514bc1e
Fernando Puente Sánchez <fpusan@gmail.com>
2021-07-05 12:35:44 +0000

    check for biopython

--- utils/install_utils/test_install.pl
+++ utils/install_utils/test_install.pl
@@ -81,6 +81,7 @@ if(!$ecode) {
 	check_python_library("matplotlib");
 	check_python_library("dendropy");
 	check_python_library("pysam");
+	check_python_library("Bio.Seq");
 	check_python_library("pandas");
 	check_python_library("cython");
         check_python_library("future");
