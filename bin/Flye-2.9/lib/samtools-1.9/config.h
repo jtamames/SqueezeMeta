c9cc72b49f7bc95ce953991f7fb126444d9d9a75
fpusan <fpusan@gmail.com>
2022-01-20 17:41:04 +0100

    actually add Flye binaries for real -_-'

---
+++ bin/Flye-2.9/lib/samtools-1.9/config.h
@@ -0,0 +1,72 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* If you use configure, this file provides #defines reflecting your
+   configuration choices.  If you have not run configure, suitable
+   conservative defaults will be used.
+
+   Autoheader adds a number of items to this template file that are not
+   used by samtools: STDC_HEADERS and most HAVE_*_H header file defines
+   are immaterial, as we assume standard ISO C headers and facilities;
+   the PACKAGE_* defines are unused and are overridden by the more
+   accurate PACKAGE_VERSION as computed by the Makefile.  */
+
+/* Define to 1 if a SysV or X/Open compatible Curses library is present */
+/* #undef HAVE_CURSES */
+
+/* Define to 1 if library supports color (enhanced functions) */
+/* #undef HAVE_CURSES_COLOR */
+
+/* Define to 1 if library supports X/Open Enhanced functions */
+/* #undef HAVE_CURSES_ENHANCED */
+
+/* Define to 1 if <curses.h> is present */
+/* #undef HAVE_CURSES_H */
+
+/* Define to 1 if library supports certain obsolete features */
+/* #undef HAVE_CURSES_OBSOLETE */
+
+/* Define to 1 if you have the `z' library (-lz). */
+#define HAVE_LIBZ 1
+
+/* Define to 1 if the Ncurses library is present */
+/* #undef HAVE_NCURSES */
+
+/* Define to 1 if the NcursesW library is present */
+/* #undef HAVE_NCURSESW */
+
+/* Define to 1 if <ncursesw/curses.h> is present */
+/* #undef HAVE_NCURSESW_CURSES_H */
+
+/* Define to 1 if <ncursesw.h> is present */
+/* #undef HAVE_NCURSESW_H */
+
+/* Define to 1 if <ncurses/curses.h> is present */
+/* #undef HAVE_NCURSES_CURSES_H */
+
+/* Define to 1 if <ncurses.h> is present */
+/* #undef HAVE_NCURSES_H */
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "samtools-help@lists.sourceforge.net"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "Samtools"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "Samtools 1.9"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "samtools"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "http://www.htslib.org/"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.9"
+
+/* Number of bits in a file offset, on hosts where this is settable. */
+/* #undef _FILE_OFFSET_BITS */
+
+/* Define for large files, on AIX-style hosts. */
+/* #undef _LARGE_FILES */
---
+++ bin/Flye-2.9/lib/samtools-1.9/htslib-1.9/htslib_static.mk
@@ -0,0 +1,2 @@
+HTSLIB_static_LDFLAGS = -lz -L/home/fer/opt/SqueezeMeta/bin/Flye-2.9/lib/minimap2 -lminimap2 -rdynamic
+HTSLIB_static_LIBS = -lpthread -lz -lm -ldl
