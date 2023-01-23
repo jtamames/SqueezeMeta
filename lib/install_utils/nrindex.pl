dfd3878c604dfcd6cbb7c7b7f36880fa8565bc47
tamames <jtamames@cnb.csic.es>
2023-01-23 16:09:23 +0100

    Making database for new databases

--- lib/install_utils/nrindex.pl
+++ lib/install_utils/nrindex.pl
@@ -9,7 +9,7 @@ use strict;
 #-- Output file. MUST POINT TO DATABASE DIRECTORY
 
 my $databasedir=$ARGV[0];
-my $nrfasta="$databasedir/nr.faa";	#-- Previously downloaded
+my $nrfasta=$ARGV[1] || "$databasedir/nr.faa";	#-- Previously downloaded
 
 my $resdir="$databasedir/LCA_tax";
 if(-d $resdir) {} else { system("mkdir $resdir"); }
