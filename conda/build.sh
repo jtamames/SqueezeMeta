b3c4471201696186ed82b5c1abce7fdce6ce8229
fpusan <fpusan@gmail.com>
2024-03-18 12:56:13 +0100

    switch to SuperPang v1.3.0

--- conda/build.sh
+++ conda/build.sh
@@ -17,8 +17,8 @@ $PYTHON setup.py install
 cpanm Linux::MemInfo -L $PREFIX
 #$PYTHON -m pip install --no-deps https://files.pythonhosted.org/packages/6f/9c/5f7d48cbf6c0400abb1a3414cfee8e557600391840f06bbb497c49082d9d/igraph-0.10.8-pp310-pypy310_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
 $PYTHON -m pip install https://files.pythonhosted.org/packages/39/d0/0bb235071b241f7294bfacc54623a98afddf855ba36807a8dae1717b5694/mOTUlizer-0.2.4.tar.gz # we let mOTUlizer pull also igraph
-$PYTHON -m pip install --no-deps https://files.pythonhosted.org/packages/f1/2a/3a5c44c25388238cb001e6968de008d765fdd9af881190388421097c84d9/speedict-0.3.11-cp310-cp310-manylinux_2_28_x86_64.whl --no-deps
-$PYTHON -m pip install --no-deps https://files.pythonhosted.org/packages/40/b9/b8fa8aa5244430270e0da993b35db855f9e383ef9e174123cd5daf90ea98/SuperPang-1.1.0.post3.tar.gz
+$PYTHON -m pip install --no-deps https://files.pythonhosted.org/packages/ab/01/82638ae1bff02885b567e9ba2b7c0a0cc5c68cd124d911fd35fc335cd56c/speedict-0.3.12-cp310-cp310-manylinux_2_28_x86_64.whl
+$PYTHON -m pip install --no-deps https://files.pythonhosted.org/packages/4d/c9/307ff96cc6740664c6efad693ff780f5d8f14f8a5cfcf19039f54b5e7c43/SuperPang-1.3.0.tar.gz
 $PYTHON -m pip install --no-deps https://files.pythonhosted.org/packages/5a/4b/2ed5ee6a35a0f9b4ec896e0b796aeb931824d82b9ca236665e5839b9a7a7/gtdbtk-2.3.2.tar.gz
 # Check that we won't break anything when manually moving libraries
 #if [ -d $PREFIX/lib/site_perl/5.26.2/ ]; then exit 1; fi # This would fail on purpose
