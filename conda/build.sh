2e7e7c1c0939c533ec59c810ea24fe209f1d86a5
fpusan <fpusan@gmail.com>
2024-05-01 13:15:27 +0200

    install speedict wheel built with lower version of glibc so it works in CentOS7

--- conda/build.sh
+++ conda/build.sh
@@ -17,7 +17,7 @@ $PYTHON setup.py install
 cpanm Linux::MemInfo -L $PREFIX
 #$PYTHON -m pip install --no-deps https://files.pythonhosted.org/packages/6f/9c/5f7d48cbf6c0400abb1a3414cfee8e557600391840f06bbb497c49082d9d/igraph-0.10.8-pp310-pypy310_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
 $PYTHON -m pip install https://files.pythonhosted.org/packages/39/d0/0bb235071b241f7294bfacc54623a98afddf855ba36807a8dae1717b5694/mOTUlizer-0.2.4.tar.gz # we let mOTUlizer pull also igraph
-$PYTHON -m pip install --no-deps https://files.pythonhosted.org/packages/ab/01/82638ae1bff02885b567e9ba2b7c0a0cc5c68cd124d911fd35fc335cd56c/speedict-0.3.12-cp310-cp310-manylinux_2_28_x86_64.whl
+$PYTHON -m pip install --no-deps https://files.pythonhosted.org/packages/88/bf/cc506602798a6ee578e9ea54b06f12ac01f084be39b0615569447e49673f/speedict-0.3.12-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
 $PYTHON -m pip install --no-deps https://files.pythonhosted.org/packages/4d/c9/307ff96cc6740664c6efad693ff780f5d8f14f8a5cfcf19039f54b5e7c43/SuperPang-1.3.0.tar.gz
 $PYTHON -m pip install --no-deps https://files.pythonhosted.org/packages/5a/4b/2ed5ee6a35a0f9b4ec896e0b796aeb931824d82b9ca236665e5839b9a7a7/gtdbtk-2.3.2.tar.gz
 # Check that we won't break anything when manually moving libraries
