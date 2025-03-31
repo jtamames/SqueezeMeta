41be4b1109b08fb7e2a04fd2a87b74906f036cc8
fpusan <fpusan@gmail.com>
2025-03-31 17:28:01 +0200

    further CRAN compliance?

--- lib/SQMtools/R/CheckMProkaryote.R
+++ lib/SQMtools/R/CheckMProkaryote.R
@@ -13,7 +13,7 @@
 #' @references Parks, Imelfort, Skennerton, Hugenholtz & Tyson (2015). 
 #' CheckM: assessing the quality of microbial genomes recovered from isolates, single cells, and metagenomes
 #' \emph{Genome Res.} \bold{25}:1043-1055.
-#' (\href{https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4484387/}{PubMed}).
+#' (\href{https://pmc.ncbi.nlm.nih.gov/articles/PMC4484387/}{PubMed}).
 #'
 #'
 #' @seealso \code{\link{USiCGs}}, \code{\link{MGOGs}} and \code{\link{MGKOs}} for an alternative set of single copy genes, and for examples on how to generate copy numbers.
