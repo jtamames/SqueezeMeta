92d6835def257c625c9f776ff429ac40cac80c98
fpusan <fpusan@gmail.com>
2026-06-29 11:36:10 +0200

    stop doing libtinfo.so.5 shenanigans and hope nothing breaks

--- conda/post-link.sh
+++ conda/post-link.sh
@@ -1,7 +1,7 @@
 # fix libs for samtools and mothur
 cd $PREFIX/lib
 ln -s libncurses.so.6 libncurses.so.5
-ln -s libtinfo.so.6 libtinfo.so.5
+#ln -s libtinfo.so.6 libtinfo.so.5 # this makes the more and less commands fail in some systems, remove and see if it's still needed
 ln -s libreadline.so.8 libreadline.so.6
 cp libboost_iostreams.so libboost_iostreams.so.1.85.0
 cp libboost_system.so libboost_system.so.1.85.0
